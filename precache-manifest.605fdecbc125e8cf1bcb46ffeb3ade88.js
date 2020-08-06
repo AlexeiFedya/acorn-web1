f2696434b512c83baeb897744e7979e077180362
AlexeiFedya <fedya5711@icloud.com>
2020-08-06 19:43:51 +0300

    Updates

---
+++ precache-manifest.605fdecbc125e8cf1bcb46ffeb3ade88.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c1554ea6c2fe0136b5622a956d9c16",
+    "url": "/acorn-web/index.html"
+  },
+  {
+    "revision": "776c7b575484cec8a41b",
+    "url": "/acorn-web/static/css/main.ffff0ea2.chunk.css"
+  },
+  {
+    "revision": "cb777d437c4a3b1c4ca2",
+    "url": "/acorn-web/static/js/2.c057e039.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/acorn-web/static/js/2.c057e039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776c7b575484cec8a41b",
+    "url": "/acorn-web/static/js/main.7118070c.chunk.js"
+  },
+  {
+    "revision": "65372123c98f15e2c163",
+    "url": "/acorn-web/static/js/runtime-main.89403734.js"
+  },
+  {
+    "revision": "8ee0d8c6e1c3d5e03df92356d3e06124",
+    "url": "/acorn-web/static/media/logotype2.8ee0d8c6.png"
+  },
+  {
+    "revision": "53e4c3b582603f55e8fba725153098c0",
+    "url": "/acorn-web/static/media/maestro-symbol.53e4c3b5.svg"
+  },
+  {
+    "revision": "543bc93b2e32281bad1ede21bb3afbdd",
+    "url": "/acorn-web/static/media/mastercard-symbol.543bc93b.svg"
+  },
+  {
+    "revision": "c1531c60148d86519515c4c3ed33fd84",
+    "url": "/acorn-web/static/media/visa.c1531c60.svg"
+  }
+]);
